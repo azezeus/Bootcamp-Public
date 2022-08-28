eba031003f12191d7b32c9b2e4957d16601bdccc
azezeus <102431204+azezeus@users.noreply.github.com>
2022-08-28 16:56:59 -0500

    Add files via upload

--- users/server.py
+++ users/server.py
@@ -8,15 +8,14 @@ app=Flask(__name__)
 def index():
     return redirect('/users')
 
-
 @app.route('/users')
 def users():
-    return render_template("users.html",users=User.get_all())
+    return render_template("read_all.html",users=User.get_all())
 
 
 @app.route('/user/new')
 def new():
-    return render_template("new_user.html")
+    return render_template("create.html")
 
 @app.route('/user/create',methods=['POST'])
 def create():
@@ -24,6 +23,32 @@ def create():
     User.save(request.form)
     return redirect('/users')
 
+@app.route('/user/show/<int:id>')
+def show(id):
+    data ={ 
+        "id":id
+    }
+    return render_template("read_one.html",user=User.get_one(data))
+
+@app.route('/user/delete/<int:id>')
+def delete(id):
+    data ={
+        "id": id
+    }
+    User.delete(data)
+    return redirect('/users')
+
+@app.route('/user/edit/<int:id>')
+def edit(id):
+    data ={
+        "id":id
+    }
+    return render_template("edit.html", user=User.get_one(data))
+
+@app.route('/user/update',methods=['POST'])
+def update():
+    User.update(request.form)
+    return redirect('/users')
 
 if __name__=="__main__":
     app.run(debug=True)
